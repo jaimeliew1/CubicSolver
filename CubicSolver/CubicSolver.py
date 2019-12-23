1d9ba92d3a1f7e102086f956b93928ccd3e7a1f2
Jaime Liew <jyli@dtu.dk>
2019-12-23 13:40:33 +0100

    create cubic class for better introspection. Problems occur when h->+/-inf.

--- CubicSolver/CubicSolver.py
+++ CubicSolver/CubicSolver.py
@@ -2,110 +2,118 @@ import numpy as np
 import cmath
 
 
-def classify(a, b, c, d):
-    '''
-    Classify the cubic into one of four categories:
-    - category 0: a = 0 (quadratic)
-    - category 1: delta^2 < 0 (monotonic, one real root)
-    - category 2: yn/h < -1 (one real root, 2 turning points)
-    - category 3: yn/h > 1 (one real root, 2 turning points)
-    - category 4: |yn/h| <= 1 (3 real roots)
-    '''
-    if a == 0:
-        return 0
+def depress(a, b, c, d):
+    p = (3 * a * c - b**2) / (3 * a**2)
+    q = (2 * b**3 - 9 * a * b * c + 27 * a**2 * d) / (27 * a**3)
 
-    delta_sqr = (b**2 - 3 * a * c) / (9 * a**2)
-
-    if delta_sqr < 0:
-        return 1
-
-    x_n = -b / (3 * a)
-    y_n = a * x_n**3 + b * x_n**2 + c * x_n + d
-    delta = np.sqrt(delta_sqr)
-    h = 2 * a * delta**3
-
-    if y_n / h < -1:
-        return 2
-    elif y_n / h > 1:
-        return 3
-    elif abs(y_n / h) <= 1:
-        return 4
-    else:
-        raise(ValueError('Cubic cannot be classified.'))
+    return p, q
 
 
-def solve_class0(a, b, c, d):
-    x1 = (-c + cmath.sqrt(c**2 - 4 * b * d)) / (2 * b)
-    x2 = (-c - cmath.sqrt(c**2 - 4 * b * d)) / (2 * b)
-
-    return [x1, x2]
+class Cubic(object):
 
 
-def solve_class1(a, b, c, d):
-    delta = np.sqrt(-(b**2 - 3 * a * c) / (9 * a**2))
-    x_n = -b / (3 * a)
-    y_n = a * x_n**3 + b * x_n**2 + c * x_n + d
-    h = 2 * a * delta**3
-
-    Z = 1 / 3 * np.arcsinh(y_n / h)
-    alpha = x_n - 2 * delta * np.sinh(Z)
-    beta = x_n + delta * (np.sinh(Z) + 1j * np.sqrt(3) * np.cosh(Z))
-    gamma = x_n + delta * (np.sinh(Z) - 1j * np.sqrt(3) * np.cosh(Z))
-
-    return [alpha, beta, gamma]
+    def __init__(self, *coefs):
+        assert len(coefs) == 4
+        assert coefs[0] != 0
+        self.solver = {
+            1: self.solve_class1,
+            2: self.solve_class2,
+            3: self.solve_class3,
+            4: self.solve_class4,
+        }
 
 
-def solve_class2(a, b, c, d):
-    delta = np.sqrt((b**2 - 3 * a * c) / (9 * a**2))
-    x_n = -b / (3 * a)
-    y_n = a * x_n**3 + b * x_n**2 + c * x_n + d
-    h = 2 * a * delta**3
+        self.coefs = coefs
+        self.p, self.q = depress(*coefs)
 
-    Z = 1 / 3 * np.arccosh(-y_n / h)
-    alpha = x_n + 2 * delta * np.cosh(Z)
-    beta = x_n - delta * (np.cosh(Z) + 1j * np.sqrt(3) * np.sinh(Z))
-    gamma = x_n - delta * (np.cosh(Z) - 1j * np.sqrt(3) * np.sinh(Z))
+    @property
+    def x_n(self):
+        a, b, c, d = self.coefs
+        return -b / (3 * a)
 
-    return [alpha, beta, gamma]
+    @property
+    def y_n(self):
+        return self.q
+
+    @property
+    def delta(self):
+        return cmath.sqrt(-self.p / 3)
+
+    @property
+    def h(self):
+        return 2 / 3 * self.p * cmath.sqrt(-self.p) / cmath.sqrt(3)
+
+    @property
+    def descriminant(self):
+        return (self.q / 2)**2 + (self.p / 3)**3
+
+    @property
+    def descriminant2(self):
+        out = self.y_n / self.h
+        if np.isnan(np.imag(out)):
+            out = np.real(out)
+        return out
+
+    def classify(self):
+        '''
+        Classify the cubic into one of four categories:
+        - category 0: a = 0 (quadratic)
+        - category 1: delta^2 < 0 (monotonic, one real root)
+        - category 2: yn/h < -1 (one real root, 2 turning points)
+        - category 3: yn/h > 1 (one real root, 2 turning points)
+        - category 4: |yn/h| <= 1 (3 real roots)
+        '''
+
+        if np.iscomplex(self.descriminant2):
+            return 1
+        elif np.real(self.descriminant2) < -1:
+            return 2
+        elif np.real(self.descriminant2) > 1:
+            return 3
+        elif abs(self.descriminant2) <= 1:
+            return 4
+        else:
+            raise (ValueError('Cubic cannot be classified.'))
+
+    def solve(self):
+        the_class = self.classify()
+        roots = self.solver[the_class]()
+        return roots
+
+    def solve_class1(self):
+        Z = 1 / 3 * np.arcsinh(np.imag(-self.descriminant2))
+        alpha = self.x_n - 2 * np.imag(self.delta) * np.sinh(Z)
+        beta = self.x_n + np.imag(self.delta) * (np.sinh(Z) +
+                                        1j * np.sqrt(3) * np.cosh(Z))
+        gamma = self.x_n + np.imag(self.delta) * (np.sinh(Z) -
+                                         1j * np.sqrt(3) * np.cosh(Z))
+        return [alpha, beta, gamma]
+
+    def solve_class3(self):
+        Z = 1 / 3 * np.arccosh(self.descriminant2)
+        alpha = self.x_n + 2 * self.delta * np.cosh(Z)
+        beta = self.x_n - self.delta * (np.cosh(Z) +
+                                        1j * np.sqrt(3) * np.sinh(Z))
+        gamma = self.x_n - self.delta * (np.cosh(Z) -
+                                         1j * np.sqrt(3) * np.sinh(Z))
+
+        return [alpha, beta, gamma]
+
+    def solve_class2(self):
+        Z = 1 / 3 * np.arccosh(-self.descriminant2)
+        alpha = self.x_n - 2 * self.delta * np.cosh(Z)
+        beta = self.x_n + self.delta * (np.cosh(Z) +
+                                        1j * np.sqrt(3) * np.sinh(Z))
+        gamma = self.x_n + self.delta * (np.cosh(Z) -
+                                         1j * np.sqrt(3) * np.sinh(Z))
 
 
-def solve_class3(a, b, c, d):
-    delta = np.sqrt((b**2 - 3 * a * c) / (9 * a**2))
-    x_n = -b / (3 * a)
-    y_n = a * x_n**3 + b * x_n**2 + c * x_n + d
-    h = 2 * a * delta**3
+        return [alpha, beta, gamma]
 
-    Z = 1 / 3 * np.arccosh(y_n / h)
-    alpha = x_n - 2 * delta * np.cosh(Z)
-    beta = x_n + delta * (np.cosh(Z) + 1j * np.sqrt(3) * np.sinh(Z))
-    gamma = x_n + delta * (np.cosh(Z) - 1j * np.sqrt(3) * np.sinh(Z))
+    def solve_class4(self):
+        Z = 1 / 3 * np.arccos(self.descriminant2)
+        alpha = self.x_n + 2 * self.delta * np.cos(Z)
+        beta = self.x_n + 2 * self.delta * np.cos(Z + 2 / 3 * np.pi)
+        gamma = self.x_n + 2 * self.delta * np.cos(Z - 2 / 3 * np.pi)
 
-    return [alpha, beta, gamma]
-
-
-def solve_class4(a, b, c, d):
-    delta = np.sqrt((b**2 - 3 * a * c) / (9 * a**2))
-    x_n = -b / (3 * a)
-    y_n = a * x_n**3 + b * x_n**2 + c * x_n + d
-    h = 2 * a * delta**3
-
-    Z = 1 / 3 * np.arccos(-y_n / h)
-    alpha = x_n + 2 * delta * np.cos(Z)
-    beta = x_n + 2 * delta * np.cos(Z + 2 / 3 * np.pi)
-    gamma = x_n + 2 * delta * np.cos(Z - 2 / 3 * np.pi)
-
-    return [alpha, beta, gamma]
-
-
-solver = [solve_class0, solve_class1, solve_class2, solve_class3, solve_class4]
-
-
-def solve(a, b, c, d):
-    Class = classify(a, b, c, d)
-    X = solver[Class](a, b, c, d)
-    return X
-
-
-if __name__ == '__main__':
-    print(solve(0, 1, 2, -3))
-    print(solve(1, -5, 19, -15))
+        return [alpha, beta, gamma]
--- test_CubicSolver.py
+++ test_CubicSolver.py
@@ -1,48 +1,55 @@
 import CubicSolver
 import numpy as np
 
-
-def test_class0():
-    x1, x2 = CubicSolver.solve(0, 1, 2, -3)
-    np.testing.assert_almost_equal(x1, 1)
-    np.testing.assert_almost_equal(x2, -3)
+# def test_class0():
+#     cubic =  CubicSolver.Cubic(0, 1, 2, -3)
+#     x1, x2, x3 = cubic.solve()
+#     np.testing.assert_almost_equal(x1, 1)
+#     np.testing.assert_almost_equal(x2, -3)
 
 
 def test_class1():
-    x1, x2, x3 = CubicSolver.solve(1, -5, 19, -15)
+    cubic =  CubicSolver.Cubic(1, -5, 19, -15)
+    x1, x2, x3 = cubic.solve()
     np.testing.assert_almost_equal(x1, 1)
     np.testing.assert_almost_equal(x2, 2 + 1j * np.sqrt(11))
     np.testing.assert_almost_equal(x3, 2 - 1j * np.sqrt(11))
 
 
 def test_class2():
-    x1, x2, x3 = CubicSolver.solve(1, -11, 35, -49)
+    cubic =  CubicSolver.Cubic(1, -11, 35, -49)
+    x1, x2, x3 = cubic.solve()
     np.testing.assert_almost_equal(x1, 7)
     np.testing.assert_almost_equal(x2, 2 - 1j * np.sqrt(3))
     np.testing.assert_almost_equal(x3, 2 + 1j * np.sqrt(3))
 
 
 def test_class3():
-    x1, x2, x3 = CubicSolver.solve(1, 1, -11, 45)
+    cubic =  CubicSolver.Cubic(1, 1, -11, 45)
+    x1, x2, x3 = cubic.solve()
     np.testing.assert_almost_equal(x1, -5)
     np.testing.assert_almost_equal(x2, 2 + 1j * np.sqrt(5))
     np.testing.assert_almost_equal(x3, 2 - 1j * np.sqrt(5))
 
 
 def test_class4():
-    x1, x2, x3 = CubicSolver.solve(1, 6, 11, 6)
+    cubic = CubicSolver.Cubic(1, 6, 11, 6)
+    x1, x2, x3 = cubic.solve()
 
     np.testing.assert_almost_equal(x1, -1)
     np.testing.assert_almost_equal(x2, -3)
     np.testing.assert_almost_equal(x3, -2)
 
 
+
 def test_1000_random_cubics():
     N = 1000
     for i in range(N):
         coeffs = np.random.randint(-100, 100, 4)
-
-        soln = CubicSolver.solve(*coeffs)
+        if coeffs[0] == 0:
+            continue
+        cubic = CubicSolver.Cubic(*coeffs)
+        soln = cubic.solve()
 
         for x in soln:
             val = np.polyval(coeffs, x)
